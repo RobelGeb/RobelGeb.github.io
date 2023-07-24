b6c02d7ee77056302c8c95415e57af36f7010339
rgebr <robelgeb@uw.edu>
2023-07-24 16:11:54 -0700

    adding jsx

---
+++ splide.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+import '@splidejs/react-splide/css';
+
+new Splide('.splide').mount( { AutoScroll } );
+
+export default () => {
+    return (
+        <Splide autoStart="true" aria-label="My Favorite Images">
+            <SplideSlide>
+                <img src="/img/java logo.png"/>
+            </SplideSlide>
+            <SplideSlide>
+                <img src="/img/pythonlogo.png"/>
+            </SplideSlide>
+        </Splide>
+    );
+}
