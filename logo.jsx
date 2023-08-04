b5ab6a2db673e8ebab98e00b1821b72c36626ff5
RobelGeb <robelgeb@uw.edu>
2023-08-04 16:27:34 -0700

    react component in separate file, realigned project cards

--- logo.jsx
+++
@@ -1,5 +0,0 @@
-import { ReactComponent as Logo } from './logo.svg';
-
-export const MenuIcon = (props) =>(
-    <svg xmlns={props.url} fill={props.fill} className={props.class}></svg>
-);
---
+++ project.jsx
@@ -0,0 +1,30 @@
+const app = document.getElementById("app")
+
+function Project({ title }) {        
+
+    return (
+        <div>
+            <a href="https://github.com/RobelGeb/cryptopricer" target="_blank">
+                <div class="proj1" >
+                    <div class="inside-proj">
+                        <h2 class="proj1title">{title}</h2>
+                    </div>
+                    <img class="proj1img" src="/img/cryptoscreenshot.png" height="325" width="600"/>
+                </div>
+            </a>
+        </div>
+    )
+}
+
+function Projects() {
+    return (
+        <div>
+
+            <Project title="CryptoPricer" />
+            <Project title="Chatter" />
+            <Project title="Lyric Finder" />
+        </div>
+    )
+}
+
+ReactDOM.render(<Projects />,  app)
